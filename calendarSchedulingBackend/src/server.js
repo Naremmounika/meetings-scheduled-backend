ab6e31e7cd95b41ba9c545ecf58aee253bc95869
mouni <naremmounika@gmail.com>
2026-02-10 16:58:38 +0530

    updated server config

--- calendarSchedulingBackend/src/server.js
+++ calendarSchedulingBackend/src/server.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 const app = require("./app");
 const sequelize = require("./config/database");
+
+const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
   console.log("Database connected");
 
-  app.listen(3000, () => {
-    console.log(`Server running at http://localhost:3000`);
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
   });
+}).catch(err => {
+  console.error("DB connection failed:", err);
 });
